27186881271028ca75a3faa4b3df024a45dd61a6
Gautier DI FOLCO <gautier.difolco@gmail.com>
2015-06-09 19:12:47 +0000

    Refactor Cell's tests

--- GameOfLife/oop/spec/cell_spec.rb
+++ GameOfLife/oop/spec/cell_spec.rb
@@ -1,6 +1,8 @@
 require 'cell'
 
 describe Cell do
-  it { expect(Cell.new(State::Dead).state).to eq(State::Dead) }
-  it { expect(Cell.new(State::Alive).state).to eq(State::Alive) }
+  describe 'state' do
+    it { expect(Cell.new(State::Dead).state).to eq(State::Dead) }
+    it { expect(Cell.new(State::Alive).state).to eq(State::Alive) }
+  end
 end
