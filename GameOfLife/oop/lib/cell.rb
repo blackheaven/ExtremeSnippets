e0806b49d4575e76832ccdd1805c277702996478
Gautier DI FOLCO <gautier.difolco@gmail.com>
2015-06-10 19:29:01 +0000

    Add neighbours

--- GameOfLife/oop/lib/cell.rb
+++ GameOfLife/oop/lib/cell.rb
@@ -3,7 +3,8 @@ require 'state'
 class Cell
   attr_reader :state
 
-  def initialize(state)
+  def initialize(state, neighbours)
     @state = state
+    @neighbours = neighbours
   end
 end
--- GameOfLife/oop/spec/cell_spec.rb
+++ GameOfLife/oop/spec/cell_spec.rb
@@ -1,8 +1,13 @@
 require 'cell'
 
 describe Cell do
-  describe 'state' do
-    it { expect(Cell.new(State::Dead).state).to eq(State::Dead) }
-    it { expect(Cell.new(State::Alive).state).to eq(State::Alive) }
+  describe :state do
+    it { expect(Cell.new(State::Dead, []).state).to eq(State::Dead) }
+    it { expect(Cell.new(State::Alive, []).state).to eq(State::Alive) }
+  end
+  describe :evolve do
+    it "Dead cell with no neighbours should stay dead" do
+      expect(Cell.new(State::Dead, []).state).to eq(State::Dead)
+    end
   end
 end
