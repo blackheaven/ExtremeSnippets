1eda631526024ee56b6be7bf0e183cb6da983c0d
Gautier DI FOLCO <gautier.difolco@gmail.com>
2015-06-13 19:33:17 +0000

    Refactor Cell's tests

--- GameOfLife/oop/spec/cell_spec.rb
+++ GameOfLife/oop/spec/cell_spec.rb
@@ -1,5 +1,12 @@
 require 'cell'
 
+def sim_cell(init_state, neighbours_state, expected_state)
+  neighbours = neighbours_state.map do |s|
+    Cell.new(s, [])
+  end
+  expect(Cell.new(init_state, neighbours).state).to eq(expected_state)
+end
+
 describe Cell do
   describe :state do
     it { expect(Cell.new(State::Dead, []).state).to eq(State::Dead) }
@@ -9,18 +16,18 @@ describe Cell do
     describe 'Dead cell' do
       describe 'No neighbours' do
         it "should stay dead" do
-          expect(Cell.new(State::Dead, []).state).to eq(State::Dead)
+            sim_cell(State::Dead, [], State::Dead)
         end
       end
       describe 'One neighbour' do
         describe 'One dead neighbour' do
           it "should stay dead" do
-            expect(Cell.new(State::Dead, [Cell.new(State::Dead, [])]).state).to eq(State::Dead)
+            sim_cell(State::Dead, [State::Dead], State::Dead)
           end
         end
         describe 'One alive neighbour' do
           it "should stay dead" do
-            expect(Cell.new(State::Dead, [Cell.new(State::Alive, [])]).state).to eq(State::Dead)
+            sim_cell(State::Dead, [State::Alive], State::Dead)
           end
         end
       end
